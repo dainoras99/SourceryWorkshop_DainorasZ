41618472384a48427bd7f7c11e976778ce243799
dainoras99 <ziukasdainoras@gmail.com>
2021-08-07 13:04:36 +0300

    Deleted unnecessary code parts

--- tests/MyFirstTest.spec.js
+++ tests/MyFirstTest.spec.js
@@ -20,43 +20,22 @@ test('duckduckgo is loading', async () => {
 });
 
 test('Test that search is working', async () => {
- // await page.goto('https://start.duckduckgo.com/');
   await startPage.initiateSearch('Test');
   const result1TextContent = await page.textContent('#r1-0');
   expect(result1TextContent).toContain('Test');
 });
 
 test('Cheat sheet working?', async () => {
- // await page.goto('https://start.duckduckgo.com/');
  await startPage.initiateSearch('microsoft word cheat sheet')
-  //await page.fill('#search_form_input_homepage', 'microsoft word cheat sheet');
-  //await page.click('#search_button_homepage');
   const isCheatSheetVisible = await page.isVisible('a[data-zci-link="cheat_sheets"]');
   const cheatSheetsTitle = await page.textContent('h3.c-base__title');
   expect(isCheatSheetVisible).toBe(true);
   expect(cheatSheetsTitle).toContain('Microsoft Word 2010');
 });
 
-/*test('Search on “Password 8” generates random password', async () => {
-
-  for(let i = 0; i<2; i++) {
- //   await page.goto('https://start.duckduckgo.com/');
-    await page.fill('#search_form_input_homepage', 'microsoft word cheat sheet');
-    await page.click('#search_button_homepage');
-    if (i == 0) {
-      const first = await page.textContent('h3.c-base__title');
-    }
-    else {
-      const second = await page.textContent('h3.c-base__title');
-    }
-  }
-})*/
 
 test('Test that short wiki is working', async () => {
- // await page.goto('https://start.duckduckgo.com/');
     await startPage.initiateSearch('shorten www.wikipedia.com');
-    //await page.fill('#search_form_input_homepage', 'shorten www.wikipedia.com');
-    //await page.click('#search_button_homepage');
     const shortenLink = await page.inputValue('#shorten-url');
     await page.goto(shortenLink);
     const url = page.url();
@@ -64,7 +43,6 @@ test('Test that short wiki is working', async () => {
 })
 
 test('check that inTitle functionality works', async () => {
-//  await page.goto('https://start.duckduckgo.com/');
   await page.waitForSelector('#logo_homepage_link');
   await page.fill('#search_form_input_homepage', 'intitle:panda');
   await page.click('#search_button_homepage');
@@ -79,7 +57,6 @@ test('check that inTitle functionality works', async () => {
 const passwordsLengths = ['8', '16', '64'];
   passwordsLengths.forEach(passwordLength => {
     test(`Generate ${passwordLength} chracters long password`, async () => {
-  //    await page.goto('https://start.duckduckgo.com/');
       await page.waitForSelector("#search_form_input_homepage");
       await page.fill('#search_form_input_homepage', ("password " + passwordLength));
       await page.click("#search_button_homepage");
@@ -91,7 +68,6 @@ const passwordsLengths = ['8', '16', '64'];
   const invalidPasswordLengths = ['7', '65'];
   invalidPasswordLengths.forEach(passwordLength => {
     test(`Fails to Generate ${passwordLength} chracters long password`, async () => {
-    //  await page.goto('https://start.duckduckgo.com/');
       await page.waitForSelector("#search_form_input_homepage");
       await page.fill('#search_form_input_homepage', ("password " + passwordLength));
       await page.click("#search_button_homepage");
